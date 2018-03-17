40844db66e0f64f174f5b8367082425482ef2fec
Bob Lam <lamh32@kennedykidshk.com>
2018-03-17 13:07:44 +0800

    Add es name for coding rules

--- src/Content.js
+++ src/Content.js
@@ -1,56 +1,125 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
-import styled from 'styled-components'
+import React, { PureComponent } from "react";
+import { Card } from "semantic-ui-react";
+import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   padding: 1.5em;
-`
+`;
 
 const Grid = styled.div`
   @media all and (min-width: 768px){
     display: grid;
-    grid-template-columns: auto auto;
-    grid-gap: 1em;
+    grid-template-columns: auto auto
+    grid-gap: 1em
   }
-`
+`;
 
-const Content = () => (
-  <Container>
-    <Grid>
-      <Card>
-        <Card.Content>
-          <Card.Header>What is my fav subject?</Card.Header>
-        <Card.Description>My favourite subject is I.C.T because I love using the computer to draw and do stuff.</Card.Description>
-        </Card.Content>
-      </Card>
-      <Card>
-        <Card.Content>
-          <Card.Header>What is my dream?</Card.Header>
-          <Card.Description>My dream is to make a app that is a combianation of gmail and google</Card.Description>
-        </Card.Content>
-      </Card>
-      <Card>
-        <Card.Content>
-          <Card.Header>What I am intrested at?</Card.Header>
-          <Card.Description>I'm intrested at art, computer sience, sience, robots and electronics.</Card.Description>
-        </Card.Content>
-      </Card>
-      <Card>
-        <Card.Content>
-          <Card.Header>When you grow up, what are you going to be?</Card.Header>
-          <Card.Description>I want to be a programmer when I grow up because I want to make apps, play them and get money at the same time.</Card.Description>
-        </Card.Content>
-      </Card>
-      <Card>
-        <Card.Content>
-          <Card.Header>How old am I?</Card.Header>
-        <Card.Description>I am <span id='myAge'></span> years old</Card.Description>
-        </Card.Content>
-      </Card>
-    </Grid>
-  </Container>
-)
+const TIMESTAMP_OFFSET = 1000;
+const DAYS_IN_YEAR = 365;
+const HOURS_IN_DAY = 24;
+const SECONDS_IN_MINUTE = 60;
+const MINUTES_IN_HOUR = 60;
+const MILLISECONDS = 1000;
 
-export default Content
+function checkYourAgeInYears(birthday) {
+  const timestampDiff = new Date() - new Date(birthday);
+
+  const diffInHours = Math.floor(
+    timestampDiff / TIMESTAMP_OFFSET / SECONDS_IN_MINUTE / MINUTES_IN_HOUR
+  );
+  const diffInDays = diffInHours / HOURS_IN_DAY;
+
+  return diffInDays / DAYS_IN_YEAR;
+}
+
+function checkHowManyDaysTowardsYourNextBirth(birthday) {
+  const timestampDiff = new Date() - new Date(birthday);
+
+  const diffInHours = Math.floor(
+    timestampDiff / TIMESTAMP_OFFSET / SECONDS_IN_MINUTE / MINUTES_IN_HOUR
+  );
+  const diffInDays = diffInHours / HOURS_IN_DAY;
+
+  return Math.abs(diffInDays);
+}
+
+class Page extends PureComponent {
+  state = {
+    counter: 0
+  };
+
+  componentDidMount() {
+    // this.countdown = setInterval(this.timer, 1000);
+    this.ageInterval = setInterval(() => {
+      const age = checkYourAgeInYears("06/09/2010");
+
+      this.setState({
+        counter: age
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.ageInterval);
+  }
+
+  render() {
+    console.info("this.state.counter", this.state.counter);
+    return (
+      <Container>
+        <Grid>
+          <Card>
+            <Card.Content>
+              <Card.Header>What is my fav subject?</Card.Header>
+              <Card.Description>
+                My favourite subject is I.C.T because I love using the computer
+                to draw and do stuff.
+              </Card.Description>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Card.Header>What is my dream?</Card.Header>
+              <Card.Description>
+                My dream is to make a app that is a combianation of gmail and
+                google
+              </Card.Description>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Card.Header>What I am intrested at?</Card.Header>
+              <Card.Description>
+                I'm intrested at art, computer sience, sience, robots and
+                electronics.
+              </Card.Description>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Card.Header>
+                When you grow up, what are you going to be?
+              </Card.Header>
+              <Card.Description>
+                I want to be a programmer when I grow up because I want to make
+                apps, play them and get money at the same time.
+              </Card.Description>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Card.Header>How old am I?</Card.Header>
+              <Card.Description>
+                I am {this.state.counter} years old<br />
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        </Grid>
+      </Container>
+    );
+  }
+}
+
+export default Page;
