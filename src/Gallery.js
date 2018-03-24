4d969e2d25f82b2ea1f3bd5ee7723cb28b59738d
Bob Lam <lamh32@kennedykidshk.com>
2018-03-24 19:19:57 +0800

    Make some more images

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,9 +1,24 @@
 import React from "react";
-import bg from "./images/certificate-of-participation.png";
+import certificate from "./images/certificate-of-participation.png";
+import winningImage from "./images/profile-pic.jpg";
+import me from "./images/me-in-year-2.png";
+import { Flex, Box } from "reflexbox";
+import styled from "styled-components";
+
+const IMAGE_WIDTH = 350;
+
+const Everything = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 const Gallery = () => (
-  <div>
-    <img src={bg} />
-  </div>
+  <Everything>
+    <img src={certificate} width={`${IMAGE_WIDTH}px`} />
+    <img src={winningImage} width={`${IMAGE_WIDTH}px`} />
+    <img src={me} width={`${IMAGE_WIDTH}px`} />
+  </Everything>
 );
 export default Gallery;
