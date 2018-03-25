514509008fbfc6d473a512c4d0d0f19a7bda58d7
Bob Lam <lamh32@kennedykidshk.com>
2018-03-25 19:56:31 +0800

    Changing some stuff

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,10 +1,10 @@
 import React from "react";
-import champion from "./images/champion.jpg";
-import winningImage from "./images/profile-pic.jpg";
+import certificate from "./images/certificate-of-participation.png";
 import me from "./images/me-doing-the-computer.jpg";
 import { Flex, Box } from "reflexbox";
 import styled from "styled-components";
 import back from "./images/back.jpg";
+import champion from "./images/champion.jpg";
 
 const IMAGE_WIDTH = 350;
 
@@ -17,8 +17,8 @@ const Everything = styled.div`
 
 const Gallery = () => (
   <Everything>
+    <img src={certificate} width={`${IMAGE_WIDTH}px`} />
     <img src={champion} width={`${IMAGE_WIDTH}px`} />
-    <img src={winningImage} width={`${IMAGE_WIDTH}px`} />
     <img src={me} width={`${IMAGE_WIDTH}px`} />
     <img src={back} width={`${IMAGE_WIDTH}px`} />
   </Everything>
