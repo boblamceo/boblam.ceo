53a957c10ecba87c8d8679b84e91154a086a3904
Bob Lam <lamh32@kennedykidshk.com>
2018-04-06 13:48:03 +0800

    Music stuff

--- src/App.js
+++ src/App.js
@@ -2,18 +2,28 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import Header from "./Header";
 import Menu from "./Menu";
+import ReactMusicPlayer from 'react-music-player';
 
-const Container = styled.div``;
+var songs = [
+  {
+    url: 'Believer.mp3',
+    cover: 'path/to/jpeg',
+    artist: {
+      name: 'Metallica',
+      song: 'Believer'
+    }
+  },
 
 class App extends Component {
   render() {
     return (
       <Container>
+        <ReactMusicPlayer songs={songs} autoplay />
         <Header />
         <Menu />
       </Container>
     );
   }
-}
+},
 
 export default App;
