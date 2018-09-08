12acda39730189cbe0dc5f868c7a0664a9db3f60
Bob Lam <boblam.ceo@gmail.com>
2018-09-08 12:12:03 +0800

    Testing if adding autoPlay

--- src/components/common/MediaScroll.js
+++ src/components/common/MediaScroll.js
@@ -52,7 +52,7 @@ const MediaScroll = ({images = []}) => {
                   imageElement
                 ) : (
                   <VideoModal>
-                    <video loop height="400px">
+                    <video autoPlay loop height="400px">
                       <source src={src} type="video/mp4" />
                     </video>
                   </VideoModal>
